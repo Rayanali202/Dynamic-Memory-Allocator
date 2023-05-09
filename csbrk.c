eb9c96990dedd1dfbc37208d9a2faab6395c5c02
Rayanali202 <89812839+Rayanali202@users.noreply.github.com>
2023-05-09 17:26:20 -0500

    Update csbrk.c

--- csbrk.c
+++ csbrk.c
@@ -1,14 +1,3 @@
-/**************************************************************************
- * C S 429 MM-lab
- * 
- * csbrk.c - A wrapper for sbrk system call. Used to keep track of calls
- * and introduce an upper limit to the amount of memory one can request at any
- * one time.
- * 
- * Copyright (c) 2021 M. Hinton. All rights reserved.
- * May not be used, modified, or copied without permission.
- **************************************************************************/
-
 #include "csbrk.h"
 #include <stdio.h>
 #include <stdlib.h>
@@ -19,11 +8,6 @@
 sbrk_block *sbrk_blocks = NULL;
 size_t sbrk_bytes;
 
-/*
- * csbrk - A wrapper for sbrk. Places a maximum on the maximum amount of memory
- * that can be requested. If tracking is enabled, keeps track of the sbrk regions
- * allocated for correctness and utilization.
- */
 void *csbrk(intptr_t increment)
 {
     if (increment > 16 * PAGESIZE)
@@ -63,10 +47,6 @@ void *csbrk(intptr_t increment)
     return ret;
 }
 
-/*
- * check_malloc_output - Checks that a payload returned by umalloc falls within
- * one of the sbrk regions.
- */
 int check_malloc_output(void *payload_start, size_t payload_length)
 {
     uint64_t start_uint = (uint64_t)payload_start;
